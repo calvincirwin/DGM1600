df78962267bd34497ebcdc45c253ba42a0931035
Calvin Irwin <calvincirwin@gmail.com>
2015-09-01 09:52:15 -0600

    VariablesAndFunctions

---
+++ DGM 1600 - Scripting/Assets/VariablesAndFunctions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VariablesAndFunctions : MonoBehaviour {
+	int myInt = 5;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		myInt = 55;
+		Debug.Log (myInt*2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
