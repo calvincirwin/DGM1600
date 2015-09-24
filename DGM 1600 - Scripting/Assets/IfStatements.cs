314f3dd3fa14edfe7c29ead2337cc663e6a896f9
Calvin Irwin <calvincirwin@gmail.com>
2015-09-24 09:04:34 -0600

    Commit

---
+++ DGM 1600 - Scripting/Assets/IfStatements.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfStatements : MonoBehaviour 
+{
+	public float coffeeTemperature = 85.0f;
+	public float hotLimitTemperature = 70.0f;
+	public float coldLimitTemperature = 40.0f;
+
+
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+	}
+}
